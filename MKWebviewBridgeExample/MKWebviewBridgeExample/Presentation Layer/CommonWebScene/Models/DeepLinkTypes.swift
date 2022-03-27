9b158df484288bdaba3a139ccce9f28ed6698d0f
kim seungmin <kimtmdals@gmail.com>
2022-03-28 00:57:59 +0900

    inital Webview example

---
+++ MKWebviewBridgeExample/MKWebviewBridgeExample/Presentation Layer/CommonWebScene/Models/DeepLinkTypes.swift
@@ -0,0 +1,13 @@
+//
+//  DeepLinkTypes.swift
+//
+
+
+import Foundation
+
+enum DeepLinkTypes: String {
+    case webview = "webview"
+    case reloadWebview = "reload-webview"
+    case closeWebview = "close-webview"
+    case unknown = ""
+}
